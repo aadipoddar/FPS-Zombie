53892938f1670aa5b2dc36d41ce93b0e571641b3
Aadi Poddar <aadipoddarmail@gmail.com>
2023-08-07 23:51:32 +0530

    RayCast Interactions

---
+++ Assets/Scripts/Player/PlayerUI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using TMPro;
+
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI promtText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void UpdateText(string promptMessage)
+    {
+        promtText.text = promptMessage;
+    }
+}
